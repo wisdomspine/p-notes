b28461985dc5b772b2c71068268cce2276bb7d79
Priest Sabo Ombugadu <sabopriest@gmail.com>
2020-11-04 15:52:47 +0100

    deleted setDefaultNotebookCover function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,12 +8,12 @@ export const createDefaultNotebook = functions.auth.user().onCreate((user, conte
     return admin.firestore().doc(`notebooks/${user.uid}/notebooks/default`).create(({name: 'Default Notebook', cover:defaultNotebookCover, description: 'This is the default notebook', permanent: true, createdAt: admin.firestore.FieldValue.serverTimestamp(), updatedAt: admin.firestore.FieldValue.serverTimestamp()}));
 })
 
-export const setDefaultNotebookCover = functions.firestore.document(`notebooks/{userId}/notebooks/{notebookId}`).onWrite((change, context) =>{
-    const data = change.before.data() || {};
-    if('cover' in data && !data.cover){
-        data.cover = defaultNotebookCover;
-    }
-});
+// export const setDefaultNotebookCover = functions.firestore.document(`notebooks/{userId}/notebooks/{notebookId}`).onWrite((change, context) =>{
+//     const data = change.before.data() || {};
+//     if('cover' in data && !data.cover){
+//         data.cover = defaultNotebookCover;
+//     }
+// });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
